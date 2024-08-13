511e6111e58eeb51c45b8fde7118e28af21f0047
Anton Postnov <4qc7@proton.me>
2024-08-13 18:21:12 +0300

    Update server.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import ContactsCollection from '../db/models/contact.js';
+import ContactsCollection from '../db/models/contacts.js';
 
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
