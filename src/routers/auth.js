f353f57bdbb9519859aefae2effdbefa1ae0744d
Anton Postnov <4qc7@proton.me>
2024-08-17 19:21:15 +0300

    Update auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { ctrlWrapper } from '../middlewares/ctrlWrapper.js';
 import { registerUserSchema } from '../validation/auth.js';
 import { registerUserController } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
