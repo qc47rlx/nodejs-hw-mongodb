c45279feb8ec6b69cdd7f2857ae91edf6ffb510f
Anton Postnov <4qc7@proton.me>
2024-08-17 13:44:23 +0300

    Update isValidId.js

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,10 +1,10 @@
 import { isValidObjectId } from 'mongoose';
-import { HttpError } from 'http-errors';
+import createHttpError from 'http-errors';
 
-export const isValidID = (req, res, next) => {
-  const { contactId } = req.params;
-  if (!isValidObjectId(contactId)) {
-    throw HttpError(404, `Not found`);
+export const isValidId = (req, res, next) => {
+  const { id } = req.params;
+  if (!isValidObjectId(id)) {
+    throw createHttpError(400, 'Bad Request');
   }
 
   next();
