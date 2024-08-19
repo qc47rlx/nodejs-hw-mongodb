11a1ea60aa7668021a9f08e6b878729dd5cf7496
Anton Postnov <4qc7@proton.me>
2024-08-19 18:29:50 +0300

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import { ContactsCollection } from '../db/models/contact.js';
+import ContactsCollection from '../db/models/contact.js';
 import { SORT_ORDER } from '../constants/index.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
